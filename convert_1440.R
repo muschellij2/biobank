9c4421e6d516b31f75982e8f986e5006cf4e7518
muschellij2 <muschellij2@gmail.com>
2017-10-12 17:53:54 -0400

    Adding to repo.

---
+++ convert_1440.R
@@ -0,0 +1,124 @@
+# convert_1440
+rm(list=ls())
+library(biobankr)
+library(dplyr)
+# set to
+# /dcl01/chatterj/data/ukbiobank/phenotype
+pheno_dir = Sys.getenv("biobank")
+
+#############################
+# Set up directories
+#############################
+root_dir = file.path(
+  pheno_dir,
+  "accelerometer")
+data_dir = file.path(
+  root_dir,
+  "intensity")
+out_dir = file.path(root_dir,
+	"collapsed")
+# think about putting in biobankr
+id_file = file.path(root_dir, 
+                    "ids.txt")
+biobank_ids = readLines(id_file)
+stub = "_90004_0_0.csv.gz"
+
+iid = as.numeric(
+  Sys.getenv("SGE_TASK_ID")
+)
+if (is.na(iid)) {
+  iid = 2
+}
+
+biobank_id = biobank_ids[iid]
+# original CSV.gz
+infile = file.path(
+	data_dir, 
+	paste0(biobank_id, stub)
+)
+
+# creating output directory
+id_out_dir = file.path(out_dir,
+	biobank_id)
+if (!dir.exists(id_out_dir)) {
+	dir.create(id_out_dir)
+}
+# adding ID to df
+add_id = function(df, biobank_id) {
+	df %>% 
+	mutate(biobank_id = biobank_id)  %>% 
+	select(biobank_id, everything())
+}
+
+
+
+# Output files
+daily_file = file.path(id_out_dir,
+	"daily_activity_1440.rds")
+activ_file = file.path(id_out_dir,
+	"activity_1440.rds")
+
+no_imp_daily_file = file.path(id_out_dir,
+	"no_imputed_daily_activity_1440.rds")
+no_imp_activ_file = file.path(id_out_dir,
+	"no_imputed_activity_1440.rds")
+
+out_files = c(daily_file, activ_file,
+	no_imp_daily_file, no_imp_activ_file)
+
+if (!all(file.exists(out_files))) {
+	# organize the data
+	df = bb_read(infile)
+
+	means = bb_1440(
+	  df, 
+	  summarize_func = "mean"
+	)
+
+	means = add_id(means, biobank_id)
+
+	saveRDS(means, file = daily_file)
+
+	means_noday = bb_1440(
+	  df, 
+	  summarize_func = "mean",
+	  summarize_over_day = TRUE
+	)
+
+	means_noday = add_id(means_noday, 
+		biobank_id)
+
+	saveRDS(means_noday, file = activ_file)
+
+
+
+	no_imp_means = bb_1440(
+	  df, 
+	  summarize_func = "mean",
+	  keep_imputed = FALSE
+	)
+
+	no_imp_means = add_id(no_imp_means, 
+		biobank_id)
+
+	saveRDS(no_imp_means, 
+		file = no_imp_daily_file)
+
+	no_imp_means_noday = bb_1440(
+	  df, 
+	  summarize_func = "mean",
+	  summarize_over_day = TRUE,
+	  keep_imputed = FALSE
+	)
+
+	no_imp_means_noday = add_id(
+		no_imp_means_noday, 
+		biobank_id)
+
+	saveRDS(no_imp_means_noday, 
+		file = no_imp_activ_file)	
+
+	
+}
+
+
