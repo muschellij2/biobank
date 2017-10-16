623b745245d80059ec81c9b65956f6ceaa49b676
muschellij2 <muschellij2@gmail.com>
2017-10-16 17:40:53 -0400

    updated demog

--- read_bb_html.R
+++ read_bb_html.R
@@ -6,6 +6,7 @@ library(readr)
 library(dplyr)
 library(xml2)
 library(rvest)
+library(tidyr)
 
 pheno_dir = Sys.getenv("biobank")
 #############################
@@ -45,23 +46,35 @@ df$Description = gsub(
 df$coding = NA
 have_coding = grepl("coding", df$Description)
 df$coding[ have_coding ] = gsub(
-  "(.*) (Uses data-coding .*)", 
+  "(.*) (Uses data-coding.*)", 
   "\\2", df$Description[have_coding])
 df$Description[ have_coding ] = gsub(
-  "(.*) (Uses data-coding .*)", 
+  "(.*) (Uses data-coding.*)", 
   "\\1", df$Description[ have_coding ])
 df$coding = gsub(
-  ".*data-coding (.*) comprises.*", 
+  ".*data-coding (.*).*", 
   "\\1", df$coding)
+df$coding = gsub(
+  "(.*) comprises.*", 
+  "\\1", df$coding)
+
 
 ##############################
 # make variable names to 
 # match pheno.tab
 ##############################
 df$varname = gsub("-", ".", df$UDI)
+
+df = df %>% 
+  separate(varname,
+    into = c("var_id", "visit", "level"),
+    remove = FALSE)
+
 df$varname = paste0("f.", df$varname)
 
 outfile = file.path(tab_dir, 
   "variable_description.csv.gz")
 
 write_csv(x = df, path = outfile)
+
+
--- subset_demog.R
+++ subset_demog.R
@@ -4,6 +4,7 @@
 rm(list = ls())
 library(readr)
 library(dplyr)
+library(tidyr)
 
 pheno_dir = Sys.getenv("biobank")
 fname = file.path(pheno_dir, "pheno.tab")
@@ -28,7 +29,8 @@ if (!file.exists(demog_file)) {
   
   stubs = c(
     "f.21001.", "f.31.", "f.52.", 
-    "f.34.", "f.53.", "f.21003.", "f.21022.",
+    "f.34.", "f.53.", "f.21003.", 
+    "f.21022.",
     "f.22001.", "f.23099.", "f.23105.", 
     "f.924.", "f.2443.", "f.6150.", 
     "f.21000.", "f.20160.", "f.6142.", 
@@ -37,22 +39,25 @@ if (!file.exists(demog_file)) {
   vis = c(0:20)
   endings = c(0:300)
   eg = expand.grid(stub = stubs, 
-                   visit = vis, end = endings)
+                   visit = vis, 
+                   end = endings)
   eg$cn = paste0(eg$stub, 
                  eg$visit, ".", eg$end)
   cn = eg$cn
   cn = c("f.eid", cn)
   keep_cn = intersect(colnames(df), cn)
   
-  demog = df[, keep_cn]
+  all_demog = df[, keep_cn]
   
   
-  write_csv(demog, path = outfile)
+  write_csv(all_demog, path = outfile)
   
 } else {
-  demog = read_csv(demog_file)
+  all_demog = read_csv(demog_file)
 }
 
+demog = all_demog %>% 
+  filter(f.eid %in% biobank_ids)
 
 desc_file = file.path(tab_dir, 
   "variable_description.csv.gz")
@@ -60,16 +65,29 @@ vars = read_csv(desc_file)
 
 run_vars = vars %>% 
   filter(varname %in% colnames(demog)) 
+renaming_file = file.path(tab_dir,
+  "demog_vars_timevarying.csv")
+ren_df = read_csv(renaming_file)
+sub_df = ren_df %>% 
+  filter(time_varying)
+non_tv = ren_df %>% 
+  filter(!time_varying)
+non_tv$non_timevar = paste0("f.", 
+  non_tv$var_id, ".0.0")
+
+
+run_vars = left_join(run_vars, ren_df, 
+  by = "var_id")
+
 coded = run_vars %>% 
-  filter(grepl("coding", tolower(Description)))
-coded$coding = gsub(
-  ".*data-coding (.*) comprises.*", 
-  "\\1", coded$Description)
+  filter(!is.na(coding)) %>% 
+  select(varname, coding, Description, 
+    Type,
+    everything())
 
-demog_accel = demog %>% 
-  filter(f.eid %in% biobank_ids)
-
-
+#######################
+# Grabbing labels
+#######################
 codings = list.files("coding.*[.]tsv",
   path = tab_dir, full.names = TRUE)
 names(codings) = gsub(
@@ -77,14 +95,99 @@ names(codings) = gsub(
   basename(codings))
 codings = lapply(codings, read_tsv)
 
-apply_coding = function(x, code_num) {
+icol = 1
+for (icol in seq(nrow(coded))) {
+  cname = coded$varname[icol]
+  code_num = coded$coding[icol]
   code_num = as.character(code_num)
   coding = codings[[code_num]]
-  code_factor = factor(x, 
+  x = demog[, cname]
+  x = unlist(x)
+  x = unname(x)
+  x = factor(x, 
     level = coding$coding,
-    labels = coding$meaning)
-
+    labels = coding$meaning)  
+  demog[, cname] = x
 }
 
+###################################
+# Reshaping to long for visit
+###################################
+d = demog
+vis_numbers = c(0:5)
+tvar = paste0(
+  "f.", sub_df$var_id)
+tvar = outer(tvar, vis_numbers, paste,
+  sep = ".")
+tvar = c(tvar)
+tvar = outer(tvar, 0:40,
+  paste, sep = ".")
+tvar = c(tvar)
+tvar = intersect(colnames(demog), tvar)
+tvar = sort(tvar)
 
-demog[,coded$varname[1]]
+d = gather_(d, 
+  key_col = "variable", 
+  value_col = "value",
+  tvar)
+
+d = d %>% 
+  separate(col = variable,
+    into = c("f", "var_id", 
+      "visit", "zero"),
+    sep = "[.]") %>% 
+  select(-f)
+
+d$visit = as.numeric(d$visit)
+d = left_join(d, ren_df, 
+  by = "var_id") %>% 
+  select(-time_varying)
+
+stopifnot(!any(is.na(d$rename)))
+
+d = d %>% 
+  group_by(var_id) %>% 
+  mutate(max_num = max(as.numeric(zero)),
+    rename = ifelse(max_num > 0,
+      paste0(rename, ".", zero),
+      rename)) %>% 
+  ungroup
+
+d = d %>% 
+  select(-var_id, -zero, -max_num)
+
+# now should be n * visits data
+d = d %>% 
+  spread(key = rename, value = value)
+
+# renaming the single time point columns
+d = d %>% 
+  rename(
+    eid = f.eid,
+    sex = f.31.0.0,
+    yob = f.34.0.0,
+    mob = f.52.0.0
+    )
+
+cancer = colnames(d)
+cancer = grepl("40008|40005", cancer)
+d = d[, !cancer]
+
+outfile = file.path(
+  tab_dir, 
+  "demographics_reformat.csv.gz")
+write_csv(d, path = outfile)
+
+first = d %>% 
+  filter(visit == 0)
+
+outfile = file.path(
+  tab_dir, 
+  paste0("demographics_reformat", 
+    "_first_visit.csv.gz"))
+write_csv(first, path = outfile)
+
+
+###############################
+# remove cancer data 
+###############################
